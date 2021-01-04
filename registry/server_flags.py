ae4d7f0e7a3ae1dabfaff056c26b718eadd84c88
Artanicus <artanicus@nocturnal.fi>
2021-01-04 22:54:11 +0200

    Pick up listen port from env

--- registry/server_flags.py
+++ registry/server_flags.py
@@ -1,6 +1,8 @@
+import os
 from absl import logging, flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('listen_addr', '[::]:50050', 'Address to listen.')
+flags.DEFINE_string('listen_addr', '[::]:' + os.getenv('PORT', '50050'), 'Address to listen.')
 flags.DEFINE_string('sentry', None, 'Sentry endpoint')
+flags.DEFINE_string('port', os.getenv('PORT', ''), '')
